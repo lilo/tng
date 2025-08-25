5c7669fd505d588191764941a3ef598cee7542f8
Sergey Lilo <svlilo@yandex.ru>
2025-08-25 23:17:51 +0600

    remove unused code

--- tng.el
+++ tng.el
@@ -370,34 +370,11 @@ WHERE id = ?"
         (dolist (fn tng--post-jump-region-functions)
           (funcall fn .start_line .end-line))))))
 
-(defun tng-list-chunks-delete ()
-  "Delete chunk."
-  (interactive)
-  (let-alist (tabulated-list-get-id)
-    (tng--delete-chunk .id))
-  (tng-list-chunks-refresh))
-
-(defun tng-list-chunks-reset-lines ()
-  "Reset chunk's lines. Set start_line=1 end_line=1"
-  (interactive)
-  (let-alist (tabulated-list-get-id)
-    (tng--update-chunk-begin-end-lines .id 1 1))
-  (tng-list-chunks-refresh))
-
-(defun tng-list-chunks-reset-lines-read-lines (new-start new-end)
-  "Reset chunk's lines. Set start_line=1 end_line=1"
-  (interactive "nNew start line: \nnNew end line: ")
-  (let-alist (tabulated-list-get-id)
-    (tng--update-chunk-begin-end-lines .id new-start new-end))
-  (tng-list-chunks-refresh))
-
 (defvar tng-list-chunks-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "RET") #'tng-list-chunks-jump)
     (define-key map (kbd "C-m") #'tng-list-chunks-jump)
     (define-key map (kbd "C-k") #'tng-list-chunks-delete)
-    (define-key map (kbd "C-r") #'tng-list-chunks-reset-lines)
-    (define-key map (kbd "r") #'tng-list-chunks-reset-lines-read-lines)
     map))
 
 (define-derived-mode tng-list-chunks-mode
